1a3c4e2239b27ffd99583a3411887ffcf626d4a1
Bladimir Alejandro Velasco Cevallos <bladimirvc54@uniandes.edu.ec>
2025-09-12 18:33:46 +0200

    Frontend - Header-Footer

--- fronted/sonib-web/src/app/app.ts
+++ fronted/sonib-web/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  // Usa el nombre REAL de tu template. Si tu archivo es "app.html", usa './app.html'.
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.scss'],
 })
-export class App {
-  protected readonly title = signal('sonib-web');
-}
+export class AppComponent {}
+
